ce74266533776ca9b0594d5778cafd124258ab82
pino223-72 <pinopinpi891@gmail.com>
2025-01-31 23:14:04 +0900

    コードかきました

--- src/main/java/net/azisaba/simpleelevator/SimpleElevator.java
+++ src/main/java/net/azisaba/simpleelevator/SimpleElevator.java
@@ -1,13 +1,18 @@
 package net.azisaba.simpleelevator;
 
+import net.azisaba.simpleelevator.listener.CommonListener;
+import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class SimpleElevator extends JavaPlugin {
+    public static SimpleElevator INSTANCE;
 
     @Override
     public void onEnable() {
-        // Plugin startup logic
+        INSTANCE = this;
+        saveDefaultConfig();
 
+        Bukkit.getPluginManager().registerEvents(new CommonListener(), this);
     }
 
     @Override
---
+++ src/main/java/net/azisaba/simpleelevator/listener/CommonListener.java
@@ -0,0 +1,120 @@
+package net.azisaba.simpleelevator.listener;
+
+import com.google.common.collect.Iterators;
+import com.google.common.collect.Lists;
+import net.azisaba.simpleelevator.SimpleElevator;
+import org.bukkit.*;
+import org.bukkit.block.Block;
+import org.bukkit.block.BlockFace;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerMoveEvent;
+import org.bukkit.event.player.PlayerToggleSneakEvent;
+import org.bukkit.util.BlockIterator;
+import org.bukkit.util.Vector;
+
+import java.util.stream.IntStream;
+
+public class CommonListener implements Listener {
+    @EventHandler
+    public void onElevatorDown(PlayerToggleSneakEvent event) {
+        Player player = event.getPlayer();
+
+        if ( !event.isSneaking() ) {
+            return;
+        }
+
+        Block baseFrom = player.getLocation().getBlock().getRelative(BlockFace.DOWN);
+        if ( !isFloor(baseFrom) ) {
+            return;
+        }
+
+        Block baseTo = tryFindFloor(baseFrom, BlockFace.DOWN);
+        if ( baseTo == null ) {
+            return;
+        }
+
+        if ( !player.hasPermission("elevator.use") ) {
+            player.sendMessage(ChatColor.RED + "あなたはエレベーターを下る権限を持っていません！");
+            return;
+        }
+
+        Location playerTo = baseTo.getRelative(BlockFace.UP).getLocation();
+
+        player.teleport(playerTo);
+        player.playSound(playerTo, Sound.ENTITY_ENDERMAN_TELEPORT, 1, 1);
+        player.getWorld().spawnParticle(Particle.TOTEM, playerTo, 50, 0.2, 0.2, 0.2, 0.5);
+    }
+
+    @EventHandler
+    public void onElevatorUp(PlayerMoveEvent event) {
+        Player player = event.getPlayer();
+        Bukkit.getLogger().info("aaa");
+        if ( player.isOnGround() ) {
+            return;
+        }
+        if ( player.isDead() ) {
+            return;
+        }
+        if ( player.isFlying() ) {
+            return;
+        }
+        if ( player.isSwimming() ) {
+            return;
+        }
+        if ( player.getVelocity().getY() <= 0 ) {
+            return;
+        }
+
+        Block baseFrom = player.getLocation().getBlock().getRelative(BlockFace.DOWN);
+        if ( !isFloor(baseFrom) ) {
+            return;
+        }
+
+        Block baseTo = tryFindFloor(baseFrom, BlockFace.UP);
+        if ( baseTo == null ) {
+            return;
+        }
+
+        if ( !player.hasPermission("elevator.use") ) {
+            player.sendMessage(ChatColor.RED + "あなたはエレベーターを上る権限を持っていません！");
+            return;
+        }
+
+        Location playerTo = baseTo.getRelative(BlockFace.UP).getLocation();
+
+        player.teleport(playerTo);
+        player.playSound(playerTo, Sound.ENTITY_ENDERMAN_TELEPORT, 1, 1);
+        player.getWorld().spawnParticle(Particle.TOTEM, playerTo, 50, 0.2, 0.2, 0.2, 0.5);
+    }
+
+    private boolean isSafe(Block block) {
+        return block.getType().isTransparent();
+    }
+
+    private boolean isFloor(Block baseFrom) {
+        if ( baseFrom.getType() != SimpleElevator.INSTANCE.getConfig().get("baseBlockType", Material.DIAMOND_BLOCK)) {
+            return false;
+        }
+        return IntStream.range(1, 3).allMatch(distance -> isSafe(baseFrom.getRelative(BlockFace.UP, distance)));
+    }
+
+    private Block tryFindFloor(Block baseFrom, BlockFace face) {
+        Vector direction = face.getDirection();
+        Location loc = baseFrom.getLocation().setDirection(direction);
+        int maxDistance = baseFrom.getWorld().getMaxHeight();
+        BlockIterator it = new BlockIterator(loc, 0, maxDistance);
+
+        Iterators.advance(it, maxDistance);
+        for ( Block baseTo : Lists.newArrayList(it) ) {
+            if ( isFloor(baseTo) ) {
+                return baseTo;
+            }
+            if ( !isSafe(baseTo) ) {
+                return null;
+            }
+        }
+        return null;
+    }
+}
