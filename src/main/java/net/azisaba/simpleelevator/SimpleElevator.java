1f88ab80b58d7f9af02a38fb6c0e722dbf42fe28
pino223-72 <pinopinpi891@gmail.com>
2025-01-31 21:36:32 +0900

    simpleエレベータ

---
+++ src/main/java/net/azisaba/simpleelevator/SimpleElevator.java
@@ -0,0 +1,17 @@
+package net.azisaba.simpleelevator;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class SimpleElevator extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
